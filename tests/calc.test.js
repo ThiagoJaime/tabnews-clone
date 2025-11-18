e4168c0c645e0dfe2adefba60c4b302a6a855b1a
ThiagoJaime <thiagojaimebarb@gmail.com>
2025-11-18 05:45:46 -0300

    primeira versao de teste

---
+++ tests/calc.test.js
@@ -0,0 +1,9 @@
+const calc = require("../models/calc");
+
+test("somar 5 + 6 é igual a 11", () => {
+  expect(calc.sum(5, 6)).toBe(11);
+});
+
+// test("nome do teste", () => {
+//   expect(() => "oi").toBe();
+// });
