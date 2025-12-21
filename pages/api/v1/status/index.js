749dd3634299ebd13f992ebf9175c849335258b0
ThiagoJaime <thiagojaimebarb@gmail.com>
2025-12-21 13:26:16 -0300

    finisi api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows[0]);
-  res.status(200).json({ status: "ok" });
+  const updateAt = new Date().toISOString();
+
+  const versionResult = await database.query("SHOW server_version;");
+  const dbVersion = versionResult.rows[0].server_version;
+
+  const dbMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const dbMaxConnections = parseInt(
+    dbMaxConnectionsResult.rows[0].max_connections,
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  const connectionsUsageResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseUsageLength = connectionsUsageResult.rows[0].count;
+
+  res.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: dbVersion,
+        max_connections: dbMaxConnections,
+        current_connections: databaseUsageLength,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,12 @@
 test("Testar endpoint de status", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.current_connections).toEqual(1);
+  console.log(responseBody);
 });
