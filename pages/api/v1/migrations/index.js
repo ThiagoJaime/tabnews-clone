2747d670271168a9a1ad8eff6fb4f6b6dc83fde0
ThiagoJaime <thiagojaimebarb@gmail.com>
2026-01-01 20:46:50 -0300

    create endpoint

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,39 @@
+import migrationRun from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database";
+
+export default async function migrations(req, res) {
+  const dbClient = await database.getNewClient();
+
+  const objectConfigDefault = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  if (req.method === "GET") {
+    const pendingMigrations = await migrationRun(objectConfigDefault);
+    await dbClient.end();
+    return res.status(200).json(pendingMigrations);
+  }
+
+  if (req.method === "POST") {
+    const migrations = await migrationRun({
+      ...objectConfigDefault,
+      dryRun: false,
+    });
+
+    await dbClient.end();
+
+    if (migrations.length > 0) {
+      return res.status(201).json(migrations);
+    }
+
+    return res.status(200).json(migrations);
+  }
+
+  return res.status(405).end();
+}
