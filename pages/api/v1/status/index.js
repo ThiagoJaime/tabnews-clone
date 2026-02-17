a2599fc87e85ff33676d8a5c4173e0718bdffb27
ThiagoJaime <thiagojaimebarb@gmail.com>
2026-02-17 11:25:04 -0300

    add  logic

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,33 +1,44 @@
-import database from "infra/database.js";
+import database from "infra/database";
+import { InternalServerError } from "infra/errors";
 
 async function status(req, res) {
-  const updateAt = new Date().toISOString();
+  try {
+    const updateAt = new Date().toISOString();
 
-  const versionResult = await database.query("SHOW server_version;");
-  const dbVersion = versionResult.rows[0].server_version;
+    const versionResult = await database.query("SHOW server_version;");
+    const dbVersion = versionResult.rows[0].server_version;
 
-  const dbMaxConnectionsResult = await database.query("SHOW max_connections;");
-  const dbMaxConnections = parseInt(
-    dbMaxConnectionsResult.rows[0].max_connections,
-  );
+    const dbMaxConnectionsResult = await database.query(
+      "SHOW max_connections;",
+    );
+    const dbMaxConnections = parseInt(
+      dbMaxConnectionsResult.rows[0].max_connections,
+    );
 
-  const databaseName = process.env.POSTGRES_DB;
-  const connectionsUsageResult = await database.query({
-    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName],
-  });
-  const databaseUsageLength = connectionsUsageResult.rows[0].count;
+    const databaseName = process.env.POSTGRES_DB;
+    const connectionsUsageResult = await database.query({
+      text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    });
+    const databaseUsageLength = connectionsUsageResult.rows[0].count;
 
-  res.status(200).json({
-    update_at: updateAt,
-    dependencies: {
-      database: {
-        version: dbVersion,
-        max_connections: dbMaxConnections,
-        current_connections: databaseUsageLength,
+    res.status(200).json({
+      update_at: updateAt,
+      dependencies: {
+        database: {
+          version: dbVersion,
+          max_connections: dbMaxConnections,
+          current_connections: databaseUsageLength,
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
+
+    res.status(500).json(publicErrorObject);
+  }
 }
 
 export default status;
