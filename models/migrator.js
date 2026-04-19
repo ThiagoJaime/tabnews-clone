f311fd2280f6ded3e3139589332b6fb83900d69f
ThiagoJaime <thiagojaimebarb@gmail.com>
2026-04-19 12:45:07 -0300

    create model migrator

---
+++ models/migrator.js
@@ -0,0 +1,44 @@
+import { join } from "node:path";
+import migrationRun from "node-pg-migrate";
+import database from "infra/database";
+
+const objectConfigDefault = {
+  dryRun: true,
+  dir: join("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+export async function listPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const pendingGetHandler = await migrationRun({
+      ...objectConfigDefault,
+      dbClient: dbClient,
+    });
+
+    return pendingGetHandler;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+export async function runMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const migrations = await migrationRun({
+      ...objectConfigDefault,
+      dbClient: dbClient,
+      dryRun: false,
+    });
+
+    return migrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
